e72a35f6794caafad9d430cfdc560b769d5f1dff
elnur004 <lnur004@gmail.com>
2022-10-12 03:00:24 +0400

    added the filter functionality to the search button

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -4,6 +4,7 @@ const title = document.getElementById('title');
 const extraName = document.getElementById('extra-name');
 const extraValue = document.getElementById('extra-value');
 const toDoList = document.getElementById('to-do-list');
+const searchTitle = document.getElementById('filter-title');
 
 const toDo = [];
 
@@ -11,7 +12,7 @@ const clearInput = (...elements) => {
   elements.forEach((el) => (el.value = ''));
 };
 
-const renderToDoUI = (toDoEl) => {
+const renderToDoUI = (filterToDoEl = '') => {
   if (toDo.length === 0) {
     toDoList.classList.remove('visible');
     return;
@@ -19,17 +20,25 @@ const renderToDoUI = (toDoEl) => {
     toDoList.classList.add('visible');
   }
 
-  const toDoElement = document.createElement('li');
-  // toDoElement.textContent = toDoEl.info.title;
-  let text = toDoEl.info.title + ` - `;
-  for (const key in toDoEl.info) {
-    if (key !== 'title') {
-      text = `${text} ${key} : ${toDoEl.info[key]}`;
-    }
-  }
+  toDoList.innerHTML = '';
 
-  toDoElement.textContent = text;
-  toDoList.append(toDoElement);
+  const filteredTodo = !filterToDoEl
+    ? toDo
+    : toDo.filter((todo) => todo.info.title.includes(filterToDoEl));
+
+  filteredTodo.forEach((todo) => {
+    const toDoElement = document.createElement('li');
+    // toDoElement.textContent = toDoEl.info.title;
+    let text = todo.info.title + ` - `;
+    for (const key in todo.info) {
+      if (key !== 'title') {
+        text = `${text} ${key} : ${todo.info[key]}`;
+      }
+    }
+
+    toDoElement.textContent = text;
+    toDoList.append(toDoElement);
+  });
 };
 
 const addToDoHandler = () => {
@@ -50,12 +59,13 @@ const addToDoHandler = () => {
   };
   toDo.push(newToDo);
   console.log(...toDo);
-  renderToDoUI(newToDo);
+  renderToDoUI();
   clearInput(title, extraName, extraValue);
 };
 
 const searchHandler = () => {
-  console.log('Searched!!!');
+  renderToDoUI(searchTitle.value);
+  clearInput(searchTitle);
 };
 
 addToDoButton.addEventListener('click', addToDoHandler);
